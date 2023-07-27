1d808071aff95b70ef9dee1f0c8a1eab0c68fc23
mouniabelkheir <mouniabelkheir@outlook.com>
2023-07-27 01:25:21 +0100

    completed assignement

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-
 // Write your code here
+let num1=0;
+let num2=0;
+const multiply= 62
+let i=2;
+while(i <= multiply){ 
+  if (multiply%i===0) {
+    num1=i;
+    num2= multiply/num1;
+  }
+  i++
+}
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 0;
+let num4 = 0;
+const mod = 4;
+let j = 1;
+while (j <= mod) {
+  num3 = j;
+  num4 = mod + num3;
+  j++;
+}
+
+
+ 
+let max = Math.max(1, 2, 3, 4, 5,20);
